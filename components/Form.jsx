df57e9ffbf4d8be5d5ed6f8766cae8027f881078
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2023-08-09 13:43:52 +0530

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -9,12 +9,12 @@ const Form = ({type,post,setPost,submitting,handleSubmit}) => {
                  </span>
           </h1>
           <p className='desc text-left max-w-md'>
-          {type} and share amazing prompts with the world, and let your imagination run wild with ant AI-powered platform.
+          {type} and share amazing ideas with the world, and let your imagination run wild with this platform.
           </p>
           <form onSubmit={handleSubmit} className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism' >
              <label>
               <span className='font-satoshi font-semibold text-base text-gray-700'> 
-                Your AI Prompt
+                Your Thought or Idea
               </span>
               <textarea 
               value={post.prompt}
